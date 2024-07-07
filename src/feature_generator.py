26cb3ede63a597769053ce0bc8570d7706222ec7
Pierre Bathoum <pierre.bathoum@icloud.com>
2024-07-07 21:02:26 +0200

    First clustering experiments - try different backbones - cluster on see_ai - plot tsne

--- src/feature_generator.py
+++ src/feature_generator.py
@@ -1,8 +1,22 @@
 import torch
 import torch.nn as nn
-from torchvision.models import resnet18,resnet34,resnet50,ResNet18_Weights,ResNet34_Weights,ResNet50_Weights, ResNet
+from torchvision.models import resnet18,resnet34,resnet50,resnet101,resnet152,ResNet18_Weights,ResNet34_Weights,ResNet50_Weights, ResNet
 from model_name import Model
+from EndoFM import vision_transformer
+from Depth_Anything_V2.depth_anything_v2.dpt import DepthAnythingV2
 
+
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
+
+depth_anything_model_configs = {
+    'vits': {'encoder': 'vits', 'features': 64, 'out_channels': [48, 96, 192, 384]},
+    'vitb': {'encoder': 'vitb', 'features': 128, 'out_channels': [96, 192, 384, 768]},
+    'vitl': {'encoder': 'vitl', 'features': 256, 'out_channels': [256, 512, 1024, 1024]},
+    'vitg': {'encoder': 'vitg', 'features': 384, 'out_channels': [1536, 1536, 1536, 1536]}
+}
+
+depth_anything_encoder = 'vitb' # or 'vits', 'vitb', 'vitg'
 
 """_summary_
     Returns:
@@ -13,19 +27,53 @@ class FeatureGenerator:
         self.model = self._get_model(model_name, pretrained)
         
         
-    def _get_model(self,model_name : Model, pretrained):
+    def _get_model(self, model_name: Model, pretrained):
         match model_name:
             case Model.RES_NET_18:
-                model = resnet18(weights='DEFAULT' if pretrained else None)    
+                model = resnet18(weights='DEFAULT' if pretrained else None)
             case Model.RES_NET_34:
                 model = resnet34(weights='DEFAULT' if pretrained else None)
             case Model.RES_NET_50:
                 model = resnet50(weights='DEFAULT' if pretrained else None)
-            
-            
+            case Model.RES_NET_101:
+                model = resnet101(weights='DEFAULT' if pretrained else None)
+            case Model.RES_NET_152:
+                model = resnet152(weights='DEFAULT' if pretrained else None)
+            case Model.ENDO_FM:
+                vit = vision_transformer.VisionTransformer()
+                weights_path = "./EndoFM/endo_fm.pth"
+                state_dict = torch.load(weights_path, map_location=torch.device('cpu'))["student"]
+
+                new_state_dict = {}
+                for k, v in state_dict.items():
+                    if k.startswith('module.backbone.'):
+                        new_key = k[len('module.backbone.'):]
+                        new_state_dict[new_key] = v
+                    else:
+                        new_state_dict[k] = v
+
+                vit.load_state_dict(new_state_dict,strict=False)
+                
+                model = vit
+            case Model.DEPTH_ANY_SMALL | Model.DEPTH_ANY_BASE | Model.DEPTH_ANY_LARGE:
+                if Model.DEPTH_ANY_SMALL:
+                    depth_anything_encoder = 'vits'
+                if Model.DEPTH_ANY_BASE:
+                    depth_anything_encoder = 'vitb'
+                else:
+                    depth_anything_encoder = 'vitl'
+                    
+                    
+                model = DepthAnythingV2(**depth_anything_model_configs[depth_anything_encoder])
+                d_a_v2_weights = f"./Depth_Anything_V2/checkpoints/depth_anything_v2_{depth_anything_encoder}.pth"
+                model.load_state_dict(
+                    torch.load(d_a_v2_weights, map_location="cpu")
+                    )
+                
         if not pretrained:
             model.apply(self._init_weights)
-        model = nn.Sequential(*list(model.children())[:-1])
+        if model_name not in (Model.ENDO_FM,Model.DEPTH_ANY_SMALL, Model.DEPTH_ANY_BASE, Model.DEPTH_ANY_LARGE):
+            model = nn.Sequential(*list(model.children())[:-1])
         return model
         
         
--- src/model_name.py
+++ src/model_name.py
@@ -5,4 +5,10 @@ class Model(Enum):
     RES_NET_18 = 1
     RES_NET_34 = 2
     RES_NET_50 = 3
+    RES_NET_152 = 4
+    RES_NET_101 = 5
+    ENDO_FM = 6
+    DEPTH_ANY_SMALL = 7
+    DEPTH_ANY_BASE = 8
+    DEPTH_ANY_LARGE = 9
     
